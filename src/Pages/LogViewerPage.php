434d03f3c842249c6cb01c73faa1403704fd69fe
ZooM <yurizoom@mail.ru>
2025-02-03 10:08:54 +0700

    в composer.json поправлен конфликт версий

--- src/Pages/LogViewerPage.php
+++ src/Pages/LogViewerPage.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace YuriZoom\MoonShineLogViewer\Pages;
 
-//use MoonShine\Laravel\Attributes\Icon;
 use MoonShine\Laravel\Pages\Page;
 use YuriZoom\MoonShineLogViewer\Components\LogViewerComponent;
 
-//#[Icon('heroicons.outline.circle-stack')]
 class LogViewerPage extends Page
 {
     public function getTitle(): string
