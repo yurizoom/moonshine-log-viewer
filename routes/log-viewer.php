1b536977d3c89ddc9279fd52961fc2d9a725f827
ZooM <yurizoom@mail.ru>
2026-01-07 02:15:20 +0700

    Если в настройках moonshine auth.middleware описан как массив, вылетала ошибка при команде php artisan route:list

--- routes/log-viewer.php
+++ routes/log-viewer.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use YuriZoom\MoonShineLogViewer\Controllers\LogViewerController;
 
+$middleware = config('moonshine.auth.middleware');
+$middleware = is_array($middleware) ? $middleware : [$middleware];
+
 Route::group([
     'prefix' => config('moonshine.route.prefix'),
     'as' => 'moonshine.',
-    'middleware' => [config('moonshine.auth.middleware'), 'web'],
+    'middleware' => [...$middleware, 'web'],
 ], function () {
     Route::get('logs/{file?}', [LogViewerController::class, 'index'])->name('log.viewer.file');
     Route::get('logs/{file}/tail', [LogViewerController::class, 'tail'])->name('log.viewer.tail');
